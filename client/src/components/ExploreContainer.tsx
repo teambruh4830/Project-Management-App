320c82e60273b3d14ec90781d1b4c23345ac05dc
samblooms <16036598+samblooms@users.noreply.github.com>
2023-10-26 14:37:55 -0500

    Merged server into bruhProject

---
+++ client/src/components/ExploreContainer.tsx
@@ -0,0 +1,14 @@
+import "./ExploreContainer.css";
+
+interface ContainerProps {}
+
+const ExploreContainer: React.FC<ContainerProps> = () => {
+  return (
+    <div id="container">
+      <strong>Halu Bruh</strong>
+      <p>This is Bruh.</p>
+    </div>
+  );
+};
+
+export default ExploreContainer;
