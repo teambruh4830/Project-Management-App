2d897a3d1cd3a0fae00c9f86e0c8d2ebb2cbb683
samblooms <16036598+samblooms@users.noreply.github.com>
2023-11-01 15:24:48 -0500

    update initDB import path

--- server/server.js
+++ server/server.js
@@ -1,29 +1,29 @@
 import express from 'express';
 import client from './dist/DB/client.js';
-// //TESTING IMPORTS
-//import initializeDB from './dist/tests/dbInit.js';
-//import User from './dist/DB/User.js';
-//import Project from './dist/DB/Project.js';
-//import Ticket from './dist/DB/Ticket.js';
-//import * as dbUtils from './dist/DB/dbUtil.js';
+//TESTING IMPORTS
+import initializeDB from './dist/utils/dbInit.js';
+import User from './dist/DB/User.js';
+import Project from './dist/DB/Project.js';
+import Ticket from './dist/DB/Ticket.js';
+import * as dbUtils from './dist/DB/dbUtil.js';
 
 const app = express();
 const port = 3000;
 
-// // Initialize the test database
-//await client.connect();
-//await initializeDB();
+// Initialize the test database
+await client.connect();
+await initializeDB();
 
-// // Get all users from the database
-//const projects = await dbUtils.getAllProjects();
-//const users = await dbUtils.getAllUsers();
-//const tickets = await dbUtils.getAllTickets();
+// Get all users from the database
+const projects = await dbUtils.getAllProjects();
+const users = await dbUtils.getAllUsers();
+const tickets = await dbUtils.getAllTickets();
 
-//const user = users[0].id;
-//const relatedProjects = await dbUtils.getProjectsByUser(user);
+const user = users[0].id;
+const relatedProjects = await dbUtils.getProjectsByUser(user);
 
-//console.log('Related projects:');
-//console.log(relatedProjects);
+console.log('Related projects:');
+console.log(relatedProjects);
 
 
 
