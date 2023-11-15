9fea28b11e3de8dd4b04012fd88463b19f0c866b
samblooms <16036598+samblooms@users.noreply.github.com>
2023-11-15 09:32:54 -0600

    Update db commands and credentials in client.ts

--- server/src/DB/client.ts
+++ server/src/DB/client.ts
@@ -1,4 +1,4 @@
-import pkg from 'pg';
+/*import pkg from 'pg';
 const { Client } = pkg;
 
 const isProduction = process.env.NODE_ENV === 'production';
@@ -6,8 +6,35 @@ const isTest = process.env.NODE_ENV === 'test';
 
 // Choose the appropriate connection string based on the environment
 const connectionString = isTest
-  ? 'postgres://localhost:5432/test_pms'
-  : process.env.DATABASE_URL || 'postgres://localhost:5432/pms';
+  ? 'postgres://localhost:5432/bruhdb_testing'
+  : process.env.DATABASE_URL || 'postgres://localhost:5432/bruhdb';
+
+const client = new Client({
+    connectionString,
+    ssl: isProduction
+    ? { rejectUnauthorized: false }
+    : undefined,
+});
+
+export default client;
+*/
+
+import pkg from 'pg';
+const { Client } = pkg;
+
+const isProduction = process.env.NODE_ENV === 'production';
+const isTest = process.env.NODE_ENV === 'test';
+
+// Connection details
+const dbConfig = {
+  user: 'bruhdb_dev', // replace with your database username
+  host: 'ec2-52-13-3-131.us-west-2.compute.amazonaws.com', // replace with the public IP of your EC2 instance
+  database: isTest ? 'bruhdb_testing' : 'bruhdb', // database name
+  password: 'bruhdb_dev_pass', // replace with your database password
+  port: 5432,
+};
+
+const connectionString = `postgres://${dbConfig.user}:${dbConfig.password}@${dbConfig.host}:${dbConfig.port}/${dbConfig.database}`;
 
 const client = new Client({
     connectionString,
