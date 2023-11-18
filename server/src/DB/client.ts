acccc122f811c6cdea96a00bb973924ff30a531b
samblooms <16036598+samblooms@users.noreply.github.com>
2023-11-18 08:29:05 -0600

    change client to use AWS DB

--- server/src/DB/client.ts
+++ server/src/DB/client.ts
@@ -1,40 +1,13 @@
-import pkg from 'pg';
-const { Client } = pkg;
-
-const isProduction = process.env.NODE_ENV === 'production';
-const isTest = process.env.NODE_ENV === 'test';
-
-// Choose the appropriate connection string based on the environment
-const connectionString = isTest
-  ? 'postgres://localhost:5432/test_pms'
-  : process.env.DATABASE_URL || 'postgres://localhost:5432/pms';
-
-const client = new Client({
-    connectionString,
-    ssl: isProduction
-    ? { rejectUnauthorized: false }
-    : undefined,
-});
-
-export default client;
-
-
 // import pkg from 'pg';
 // const { Client } = pkg;
 
 // const isProduction = process.env.NODE_ENV === 'production';
 // const isTest = process.env.NODE_ENV === 'test';
 
-// // Connection details
-// const dbConfig = {
-//   user: 'bruhdb_dev', // replace with your database username
-//   host: 'localhost', // replace with the public IP of your EC2 instance
-//   database: isTest ? 'bruhdb_testing' : 'bruhdb', // database name
-//   password: 'bruhdb_dev_pass', // replace with your database password
-//   port: 5432,
-// };
-
-// const connectionString = `postgres://${dbConfig.user}:${dbConfig.password}@${dbConfig.host}:${dbConfig.port}/${dbConfig.database}`;
+// // Choose the appropriate connection string based on the environment
+// const connectionString = isTest
+//   ? 'postgres://localhost:5432/test_pms'
+//   : process.env.DATABASE_URL || 'postgres://localhost:5432/pms';
 
 // const client = new Client({
 //     connectionString,
@@ -44,3 +17,30 @@ export default client;
 // });
 
 // export default client;
+
+
+import pkg from 'pg';
+const { Client } = pkg;
+
+const isProduction = process.env.NODE_ENV === 'production';
+const isTest = process.env.NODE_ENV === 'test';
+
+// Connection details
+const dbConfig = {
+  user: 'bruhdb_dev', // replace with your database username
+  host: 'localhost', // replace with the public IP of your EC2 instance
+  database: isTest ? 'bruhdb_testing' : 'bruhdb', // database name
+  password: 'bruhdb_dev_pass', // replace with your database password
+  port: 5432,
+};
+
+const connectionString = `postgres://${dbConfig.user}:${dbConfig.password}@${dbConfig.host}:${dbConfig.port}/${dbConfig.database}`;
+
+const client = new Client({
+    connectionString,
+    ssl: isProduction
+    ? { rejectUnauthorized: false }
+    : undefined,
+});
+
+export default client;
