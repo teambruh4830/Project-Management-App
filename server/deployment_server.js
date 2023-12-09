aa0b9dac1cc566df216ca28b9370975b25ba6b85
samblooms <16036598+samblooms@users.noreply.github.com>
2023-12-09 08:53:19 -0600

    separate deployment server and testing server files

---
+++ server/deployment_server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import apiRoutes from './dist/API/API.js'; // Adjust if your routes are defined differently
+
+dotenv.config();
+const app = express();
+app.use(express.json()); // Middleware for JSON body parsing
+
+// Use your API routes
+app.use('/api', apiRoutes);
+
+// Default route
+app.get('/', (req, res) => {
+    res.send('Welcome to the API!');
+});
+
+export default app; // Export the app
