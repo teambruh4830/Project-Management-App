466f865e510eb6eae4cfcdbf0007441ff4d1d965
samblooms <16036598+samblooms@users.noreply.github.com>
2023-12-09 08:58:40 -0600

    update dbUtils for new routes

--- server/src/DB/dbUtil.ts
+++ server/src/DB/dbUtil.ts
@@ -7,6 +7,35 @@ import Project from '../DB/Project';
 import User from '../DB/User';
 import Ticket from '../DB/Ticket';
 
+
+async function userExists(username: string): Promise<boolean> {
+    const result = await client.query('SELECT id FROM users WHERE username = $1', [username]);
+    return result.rows.length > 0;
+}
+
+async function createUser(username: string, password: string): Promise<User> {
+    if (await userExists(username)) {
+        throw new Error('User already exists');
+    }
+
+    await client.query('INSERT INTO users (username, password) VALUES ($1, $2)', [username, password]);
+    const result = await client.query('SELECT * FROM users WHERE username = $1', [username]);
+    const newUserProps: UserProps = result.rows[0];
+    return new User(newUserProps);
+}
+
+async function verifyUser(username: string, password: string): Promise<User | null> {
+    const result = await client.query('SELECT * FROM users WHERE username = $1', [username]);
+    if (result.rows.length === 0) {
+        return null; // User not found
+    }
+    const userProps: UserProps = result.rows[0];
+    if (userProps.password !== password) {
+        return null; // Password does not match
+    }
+    return new User(userProps); // User verified
+}
+
 // Fetch all projects that a user is a member of from the database and return an array of Project instances
 async function getProjectsByUser(user_id: number): Promise<Project[]> {
     const { rows } = await client.query('SELECT * FROM projects WHERE id IN (SELECT project_id FROM user_projects WHERE user_id = $1)', [user_id]);
@@ -18,6 +47,112 @@ async function getTicketsByProject(project_id: number): Promise<Ticket[]> {
     const { rows } = await client.query('SELECT * FROM tickets WHERE project_id = $1', [project_id]);
     return rows.map((row: TicketProps) => new Ticket(row));
 }
+
+async function createNewProject(title: string, description: string): Promise<Project> {
+    const { rows: [newProjectRow] } = await client.query(`
+        INSERT INTO projects (title, description)
+        VALUES ($1, $2)
+        RETURNING *
+    `, [title, description]);
+
+    return new Project({
+        id: newProjectRow.id,
+        title: newProjectRow.title,
+        description: newProjectRow.description,
+        created_at: newProjectRow.created_at,
+        modified_at: newProjectRow.modified_at
+    });
+}
+
+async function addUserToProjectByUsername(username: string, projectId: number): Promise<void> {
+    const userRes = await client.query('SELECT id FROM users WHERE username = $1', [username]);
+    if (userRes.rows.length === 0) {
+        throw new Error('User not found');
+    }
+    const userId = userRes.rows[0].id;
+    await client.query('INSERT INTO user_projects (user_id, project_id) VALUES ($1, $2)', [userId, projectId]);
+}
+
+async function removeUserFromProjectByUsername(username: string, projectId: number): Promise<void> {
+    const userRes = await client.query('SELECT id FROM users WHERE username = $1', [username]);
+    if (userRes.rows.length === 0) {
+        throw new Error('User not found');
+    }
+    const userId = userRes.rows[0].id;
+    await client.query('DELETE FROM user_projects WHERE user_id = $1 AND project_id = $2', [userId, projectId]);
+}
+
+
+
+// Create a new ticket in the database
+async function createNewTicket(ticketData: TicketProps): Promise<Ticket> {
+    const { project_id, created_by, assigned_to, type, priority } = ticketData;
+    const { rows: [newTicketRow] } = await client.query(`
+        INSERT INTO tickets (project_id, created_by, assigned_to, type, priority, created_at, modified_at)
+        VALUES ($1, $2, $3, $4, $5, NOW(), NOW())
+        RETURNING *
+    `, [project_id, created_by, assigned_to, type, priority]);
+
+    return new Ticket(newTicketRow);
+}
+
+// Update an existing project in the database
+async function updateProject(projectData: ProjectProps): Promise<Project> {
+    const { id, title, description } = projectData;
+    const { rows: [updatedProjectRow] } = await client.query(`
+        UPDATE projects
+        SET title = $1, description = $2, modified_at = NOW()
+        WHERE id = $3
+        RETURNING *
+    `, [title, description, id]);
+
+    return new Project(updatedProjectRow);
+}
+
+async function updateTicket(ticketData: TicketProps): Promise<Ticket> {
+    try {
+        // Fetch the existing ticket data
+        const { rows: existingTicketRows } = await client.query('SELECT * FROM tickets WHERE id = $1', [ticketData.id]);
+        if (existingTicketRows.length === 0) {
+            throw new Error('Ticket not found');
+        }
+        const existingTicket = existingTicketRows[0];
+
+        // Merge existing data with new data
+        const updatedData = {
+            ...existingTicket,
+            ...ticketData,
+            modified_at: new Date() // Update the modified_at timestamp
+        };
+
+        // Update the ticket in the database
+        const { rows: [updatedTicketRow] } = await client.query(`
+            UPDATE tickets
+            SET project_id = $1, created_by = $2, assigned_to = $3, type = $4, priority = $5, modified_at = $6
+            WHERE id = $7
+            RETURNING *
+        `, [updatedData.project_id, updatedData.created_by, updatedData.assigned_to, updatedData.type, updatedData.priority, updatedData.modified_at, updatedData.id]);
+
+        return new Ticket(updatedTicketRow);
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function deleteProject(projectId: number): Promise<void> {
+    await client.query('DELETE FROM tickets WHERE project_id = $1', [projectId]);
+    await client.query('DELETE FROM user_projects WHERE project_id = $1', [projectId]);
+    await client.query('DELETE FROM projects WHERE id = $1', [projectId]);
+}
+
+async function deleteTicket(ticketId: number): Promise<void> {
+    await client.query('DELETE FROM tickets WHERE id = $1', [ticketId]);
+}
+
+
+
+
+
 
 
 //------------------------------------------------------------
@@ -78,9 +213,21 @@ async function saveAllProjects(projects: Project[]) {
     }
 }
 
+
 //------------------------------------------------------------
 
 export {
+    userExists,
+    createUser,
+    verifyUser,
+    createNewProject,
+    createNewTicket,
+    updateProject,
+    updateTicket,
+    deleteProject,
+    deleteTicket,
+    addUserToProjectByUsername,
+    removeUserFromProjectByUsername,
     getProjectsByUser,
     getTicketsByProject,
     getAllUsers,
