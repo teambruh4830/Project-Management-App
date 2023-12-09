4505137d72f4e055ced97b16790659b77f2665a5
samblooms <16036598+samblooms@users.noreply.github.com>
2023-12-09 14:20:43 -0600

    update cors compatibilitiy

--- server/deployment_server.js
+++ server/deployment_server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import apiRoutes from './dist/API/API.js'; // Adjust if your routes are defined differently
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json()); // Middleware for JSON body parsing
 
 // Use your API routes
